ec29287b7263231538375871944415ee8382b853
Dodontak <110716928+Dodontak@users.noreply.github.com>
2024-04-12 14:58:54 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    scanf("%i %i",&a,&b);
+    printf("%i\n",a+b);
+    printf("%i\n",a-b);
+    printf("%i\n",a*b);
+    printf("%i\n",a/b);
+    printf("%i\n",a%b);
+    return 0;
+}
