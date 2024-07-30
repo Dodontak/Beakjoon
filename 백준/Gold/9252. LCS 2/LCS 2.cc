4b29e1998d9524eec5faece5767bbf494d5689ef
Dodontak <110716928+Dodontak@users.noreply.github.com>
2024-07-31 03:22:21 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6176 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	string	str1, str2;
+	cin >> str1 >> str2;
+	vector<vector<int> >	vec(str1.size() + 1, vector<int>(str2.size() + 1, 0));
+	for (int i = 0; i < str1.size(); ++i) {
+		for (int j = 0; j < str2.size(); ++j) {
+			if (str1[i] == str2[j]) {
+				vec[i + 1][j + 1] = vec[i][j] + 1;
+			} else {
+				vec[i + 1][j + 1] = max(vec[i + 1][j], vec[i][j + 1]);
+			}
+		}
+	}
+	stack<char>	ans;
+	int	max_len = vec.back().back();
+	int	y = str1.size();
+	int	x = str2.size();
+	while (y > 0 && x > 0) {
+        if (vec[y][x] == vec[y - 1][x]) {
+            y--;
+        } else if (vec[y][x] == vec[y][x - 1]) {
+            x--;
+        } else {
+            ans.push(str1[y - 1]);
+            y--;
+            x--;
+        }
+    }
+	cout << vec.back().back() << endl;
+	while (!ans.empty()) {
+		cout << ans.top();
+		ans.pop();
+	}
+	cout << endl;
+}
