8f785b2d07cd19dd27e41ee088b98cd57432488f
Dodontak <110716928+Dodontak@users.noreply.github.com>
2024-11-11 20:07:07 +0900

    [Silver II] Title: 나무 자르기, Time: 132 ms, Memory: 6120 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,39 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    long long n, m, max = 0;
-    int tree[1000001];
-    
-    cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> tree[i];
-    
-    sort(tree, tree + n);
-    
-    long long low = 0;
-    long long high = tree[n - 1];
-    
-    while(low <= high){ //cut 가능할 때 까지
-        long long sum = 0;
-        long long cut = (low + high) / 2;
-        
-        for(int i = 0; i < n; i++) {
-            if(tree[i] - cut > 0) sum += tree[i] - cut; // cut 하고 남는 게 있다면 가져감
-        }
-        
-        if(sum >= m){ // m미터보다 가져간 나무가 같거나 많으면
-            max = cut; // 현재 cut 지점을 최대 지점으로 저장
-            low = cut + 1; // cut 가능 구간을 더 올림
-        } else{
-            high = cut - 1; // m미터가 안 되면 cut 가능 구간을 내림
-        }
-    }
-    
-    cout << max; // 최대 cut 지점 출력
-    
-    return 0;
+long long get_length(const vector<int> &tree, int h)
+{
+	long long len = 0;
+	for (int i = 0; i < tree.size(); ++i)
+		if (tree[i] > h)
+			len += tree[i] - h;
+	return len;
+}
+
+int get_saw_heitht(const int &max_len, const long long &want_length, const vector<int> &tree)
+{
+	int st = 0;
+	int ed = max_len;
+	int ans;
+	while (st <= ed)
+	{
+		int mid = ((long long)st + ed) / 2;
+		long long len = get_length(tree, mid);
+		if (len >= want_length)
+		{
+			ans = mid;
+			st = mid + 1;
+		}
+		else
+		{
+			ed = mid - 1;
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n, m;
+	cin >> n >> m;
+	vector<int> tree(n);
+	int max_len = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> tree[i];
+		max_len = max(max_len, tree[i]);
+	}
+	cout << get_saw_heitht(max_len, m, tree) << endl;
 }
