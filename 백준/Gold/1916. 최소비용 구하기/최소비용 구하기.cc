f2fe6224883d422b04f5c09a41222dab4f769468
Dodontak1 <110716928+Dodontak@users.noreply.github.com>
2026-02-11 07:47:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 16 ms, Memory: 4848 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,51 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 #define	INF	2147483647
 
 using namespace std;
 
-typedef	pair<int, int>	pii;
-typedef priority_queue<pii, vector<pii>, greater<pii> >	PQ;
+typedef pair<int, int> PII;
+typedef priority_queue<PII, vector<PII>, greater<PII> >	PQ;
 
-int	dijkstra(int start, int end, const vector<vector<pii> > &links) {
-	PQ	pq;
+int dijkstra(int start, int end, const vector<vector<PII>>& links)
+{
+	PQ pq;
 	pq.push(make_pair(0, start));
-	vector<int>	dists(links.size(), INF);
+	vector<int> dists(links.size(), INF);
 	dists[start] = 0;
-	while (!pq.empty()) {
-		int	cur_node = pq.top().second;
-		int	cur_dist = pq.top().first;
+	while (!pq.empty())
+	{
+		int cur_node = pq.top().second;
+		int cur_dist = pq.top().first;
 		pq.pop();
-		if (cur_node == end) break;
+		if (cur_node == end) return cur_dist;
 		if (cur_dist != dists[cur_node]) continue;
-		for (int i = 0; i < links[cur_node].size(); ++i) {
-			int	linked_node = links[cur_node][i].second;
-			int	link_dist = links[cur_node][i].first;
-			int	new_dist = cur_dist + link_dist;
-			if (new_dist < dists[linked_node]) {
-				dists[linked_node] = new_dist;
-				pq.push(make_pair(new_dist, linked_node));
+		for (int i = 0; i < links[cur_node].size(); i++)
+		{
+			int next_node = links[cur_node][i].first;
+			int next_dist = links[cur_node][i].second + dists[cur_node];
+			if (next_dist < dists[next_node])
+			{
+				dists[next_node] = next_dist;
+				pq.push(make_pair(next_dist, next_node));
 			}
 		}
 	}
-	return (dists[end]);
+	return -1;
 }
 
-int main(void)
+int main()
 {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cout.tie(0);
-	int	n, m;
+
+	int n, m;
 	cin >> n >> m;
-	vector<vector<pii> >	links(n + 1);
-	for (int i = 0; i < m; ++i) {
-		int	st, ed, dist;
-		cin >> st >> ed >> dist;
-		links[st].push_back(make_pair(dist, ed));
+	vector<vector<PII>> links(n + 1);
+	for (int i = 0; i < m; i++)
+	{
+		int st, ed, cost;
+		cin >> st >> ed >> cost;
+		links[st].push_back(make_pair(ed, cost));
 	}
-	int	start, end;
+	int start, end;
 	cin >> start >> end;
 	cout << dijkstra(start, end, links) << endl;
 }
