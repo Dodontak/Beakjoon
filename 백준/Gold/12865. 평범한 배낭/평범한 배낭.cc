fb89cf60cf19baa9c977f6d1cd4c5858f68a0410
Dodontak1 <110716928+Dodontak@users.noreply.github.com>
2026-03-27 11:43:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 24 ms, Memory: 2840 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -8,13 +8,12 @@ int main() {
   int n, k;
   cin >> n >> k;
   vector<int> dp(k + 1, 0);
-  vector<int> new_dp;
+  vector<int> new_dp(k + 1);
   vector<pair<int, int>> stuffs(n + 1);
   for (int i = 1; i <= n; i++)
     cin >> stuffs[i].first >> stuffs[i].second;
 
   for (int y = 1; y <= n; y++) {
-	new_dp.reserve(k + 1);
     for (int x = 0; x <= k; x++) {
       if (x - stuffs[y].first >= 0)
         new_dp[x] = max(dp[x], dp[x - stuffs[y].first] + stuffs[y].second);
