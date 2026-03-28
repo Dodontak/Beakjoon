d64308e2034a2e051bb83e565114988fab95fd38
Dodontak1 <110716928+Dodontak@users.noreply.github.com>
2026-03-28 11:09:03 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 12 ms, Memory: 3708 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,56 +1,57 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-int	bfs(int n, vector<bool>& visit, const int& k)
+// pair<time, pos>
+using PII = pair<int, int>;
+using PQ = priority_queue<PII, vector<PII>, greater<PII>>;
+
+bool can_visit_pos(int pos, const vector<bool> &visit)
 {
-	queue<pair<int, int> >	q1;
-	queue<pair<int, int> >	q2;
-	q1.push(make_pair(n, 0));
-	visit[n] = true;
-	int	pos;
-	int	count;
-	while (!q1.empty() || !q2.empty()) {
-		if (!q1.empty()) {
-			pos = q1.front().first;
-			count = q1.front().second;
-			q1.pop();
-		} else {
-			pos = q2.front().first;
-			count = q2.front().second;
-			q2.pop();
-		}
-		if (pos == k) {
-			return (count);
-		}
-		if (pos * 2 <= 200000 && visit[pos * 2] == false) {
-			if (pos * 2 == k) return (count);
-			visit[pos * 2] = true;
-			q1.push(make_pair(pos * 2, count));
-		}
-		if (pos - 1 >= 0 && visit[pos - 1] == false) {
-			if (pos - 1 == k) return (count + 1);
-			visit[pos - 1] = true;
-			q2.push(make_pair(pos - 1, count + 1));
-		}
-		if (pos + 1 <= 200000 && visit[pos + 1] == false) {
-			if (pos + 1 == k) return (count + 1);
-			visit[pos + 1] = true;
-			q2.push(make_pair(pos + 1, count + 1));
-		}
-	}
-	return (-1);
+    if (pos < 0 || pos >= visit.size())
+        return false;
+    if (visit[pos] == true)
+        return false;
+    return true;
 }
 
-int	main()
+int func(int start, int dest)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int	n; cin >> n;
-	int	k; cin >> k;
-	vector<bool>	visit(200001, false);
-	cout << bfs(n, visit, k) << endl;
+    if (start >= dest)
+        return start - dest;
+    PQ pq;
+    vector<bool> visit(dest * 3, false);
+    pq.push({0, start});
+    visit[start] = true;
+    while (!pq.empty())
+    {
+        int cur_pos = pq.top().second;
+        int cur_time = pq.top().first;
+        pq.pop();
+        visit[cur_pos] = true;
+        if (cur_pos == dest)
+            return cur_time;
+        if (can_visit_pos(cur_pos + 1, visit))
+        {
+            pq.push({cur_time + 1, cur_pos + 1});
+        }
+        if (can_visit_pos(cur_pos - 1, visit))
+        {
+            pq.push({cur_time + 1, cur_pos - 1});
+        }
+        if (can_visit_pos(cur_pos * 2, visit))
+        {
+            pq.push({cur_time, cur_pos * 2});
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    cout << func(n, k) << endl;
 }
